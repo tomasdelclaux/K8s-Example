721188e916d5b83a668d65af46aeb3bf3ba29f13
Tomas Delclaux <tomas@Tomass-iMac.local>
2020-05-03 23:11:31 +0200

    exercise files added

---
+++ client/src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  // Commented out because the frontend sends async call to express server and express server might not be ready when test run
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div);
+});
